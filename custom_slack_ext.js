02979bf89d530b48f8337a522309b4fe531623f4
Alon Harel <alonh@duda.co>
2025-06-03 02:20:11 +0300

    start

--- custom_slack_ext.js
+++ custom_slack_ext.js
@@ -403,7 +403,7 @@ try {
                 }
                 
                 const requestBody = {
-                    model: 'gpt-3.5-turbo',
+                    model: 'gpt-4o',
                     messages: [
                         {
                             role: 'user',
